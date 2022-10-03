c4306013ba5be7389907eecd4bb7fa840ab0aa30
Krishna <krishna.prasanna_kumar@kcl.ac.uk>
2022-10-03 20:27:33 +0100

    first try

--- primes.py
+++ primes.py
@@ -3,4 +3,13 @@
 
 def primes(number_of_primes):
     list = []
+    list[0] = 2
+    if number_of_primes == 1:
+        return list
+    else:
+        for x in range(2,number_of_primes):
+            for i in range(x):
+                if (x%i) == 0:
+                    list.append(x)
+
     return list
